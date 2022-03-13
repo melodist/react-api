a63761a6314593f0c3ea3b0bc4e1dc5403eb5aa2
MELT <melt.ep00@gmail.com>
2022-03-13 19:20:08 +0900

    Feat: CORS 설정 및 환경변수 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
+ 
 function App() {
   const [count, setCount] = useState(0)
+  function getHello() {
+    const greet = document.getElementById('greet')
+    fetch(import.meta.env.VITE_API_SERVER + '/api/hello')
+      .then(response => response.json())
+      .then(data => greet.innerHTML = JSON.stringify(data))
+  }
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(getHello)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>React API Server</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
+        </p>
+        <p>
+          api called: <code id="greet"></code>
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
